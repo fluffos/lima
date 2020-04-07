3804e5142c498a8670f3d7a2052eb1e5a1cd2aad
Yucong Sun <fallentree@fb.com>
2020-04-07 12:28:24 -0700

    fix issue in build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 set -euo pipefail
 
 rm -rf build
-mkdir -p build && cd build
+mkdir -p build
 
 # fix me, should use cmake variables
-cp -f local_options driver/src/
+cp -f local_options ./driver/src/
 
+cd build
 cmake ../driver -DPACKAGE_UIDS=OFF
 
 make install
+cd ..
