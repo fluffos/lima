332aaa89c5447c19a2b69bed440363d6120c675b
gesslar <karahd@gmail.com>
2022-10-23 11:20:24 -0400

    correcting the doc for set_fill_action (had wrong function name) and also some formatting updates

--- lib/std/modules/m_fluid_source.c
+++ lib/std/modules/m_fluid_source.c
@@ -22,25 +22,25 @@ mixed fill_action = "$N $vdip $p $o into $o1 and $vwithdraw some $o2.";
 //x must be a path name to a fluid object.
 //new(x) is what is moved into a container
 //when the container is filled from here.
-void set_source_of( string what  ){
-  object ob;
-  string *ids;
+void set_source_of( string what  ) {
+    object ob;
+    string *ids;
 
-  ob = new( what );
-  ids = ob -> query_id();
+    ob = new( what );
+    ids = ob -> query_id();
 
-  foreach( string id in ids ) add_id( id + "_source" );
-  add_id ( "fluid_source" );
-  add_id ( "fill_source" );
-  add_id ( "drink_source" );
+    foreach( string id in ids ) add_id( id + "_source" );
+    add_id ( "fluid_source" );
+    add_id ( "fill_source" );
+    add_id ( "drink_source" );
 
-  source_of = what;
-  
+    source_of = what;
 }
 
-string is_source_of(){
-return source_of;
-}//this is the function that is queried
+string is_source_of() {
+    return source_of;
+}
+//this is the function that is queried
 //when the fluid object is cloned.
 //A clever coder in need could have several fluids
 //from the same source by overriding this function.
@@ -54,65 +54,63 @@ return source_of;
 //disturbed.  As a string, x is passed
 //to my_action.  If you don't want any
 //disturb_action, set x to 0.
-void set_fluid_disturb_action( mixed x ){
-  fluid_disturb = x;
+void set_fluid_disturb_action( mixed x ) {
+    fluid_disturb = x;
 }
 
-mixed get_fluid_disturb_action(){
-  return fluid_disturb;
+mixed get_fluid_disturb_action() {
+    return fluid_disturb;
 }
 
 //:FUNCTION set_fill_action
-//set_fluid_disturb_action( string x )
-//'x' is passed to simple_action. 
+//set_fill_action( string x )
+//'x' is passed to simple_action.
 //$o in the string represents the fluid container.
 //$o1 represents the fluid source.
 //$o2 represents the fluid itself.
-void set_fill_action( string x ){
-  fill_action = x;
+void set_fill_action( string x ) {
+    fill_action = x;
 }
 
-mixed fill_up(object bottle){
-	object fluid;
+mixed fill_up(object bottle) {
+    object fluid;
 
-	fluid = new(is_source_of() );
+    fluid = new(is_source_of() );
 
-	if (bottle -> fill_with( fluid ))
+    if (bottle -> fill_with( fluid ))
 
-	{
-	this_body() -> simple_action
-	( fill_action, bottle -> short(), this_object() -> the_short(), fluid);
-	
-	if (fluid_disturb) this_body() -> my_action( fluid_disturb, fluid, this_object() );
+    {
+    this_body() -> simple_action
+    ( fill_action, bottle -> short(), this_object() -> the_short(), fluid);
 
-	return 1;
-	}
+    if (fluid_disturb) this_body() -> my_action( fluid_disturb, fluid, this_object() );
 
-	else return 0;
+    return 1;
+    }
+
+    else return 0;
 }
 
 // Verb stuff:
 
-
-mixed direct_drink_obj(){
-return "#You cannot drink an entire " + query_primary_id();
+mixed direct_drink_obj() {
+    return "#You cannot drink an entire " + query_primary_id();
 }
 
-mixed indirect_drink_str_from_obj( string fluid, object ob ){
-if ( !member_array( fluid+"_source", query_id() )) return "You cannot drink "+fluid+" from that.\n";
-return 1;
+mixed indirect_drink_str_from_obj( string fluid, object ob ) {
+    if ( !member_array( fluid+"_source", query_id() )) return "You cannot drink "+fluid+" from that.\n";
+    return 1;
 }
 
-mixed direct_drink_str_from_obj( string fluid, object ob ){
-if ( !member_array( fluid+"_source", query_id() )) return "You cannot drink "+fluid+" from that.\n";
-return 1;
+mixed direct_drink_str_from_obj( string fluid, object ob ) {
+    if ( !member_array( fluid+"_source", query_id() )) return "You cannot drink "+fluid+" from that.\n";
+    return 1;
 }
 
-int indirect_fill_obj_from_obj(){
-return 1;
+int indirect_fill_obj_from_obj() {
+    return 1;
 }
 
-mixed indirect_drink_obj_from_obj()
-{
-return 1;
+mixed indirect_drink_obj_from_obj() {
+    return 1;
 }
