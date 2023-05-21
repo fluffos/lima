60e5f7498846fe86627bb23cff61cc9e474f4dd7
Tsathoggua of NKai <sstaunp+github@gmail.com>
2023-05-21 15:12:11 +0000

    PORTAL message fixes

--- lib/std/portal.c
+++ lib/std/portal.c
@@ -1,59 +1,83 @@
 /* Do not remove the headers from this file! see /USAGE for more info. */
 
-//: MODULE
+//:MODULE
 // A simple one way portal
 
 inherit COMPLEX_EXIT_OBJ;
 inherit M_GETTABLE;
 
 string look_in_desc;
+string destination;
 
-//: FUNCTION set_destination
+string enter_msg="$N $vleave through the $o.";
+string leave_msg="$N $venter through the $o.";
+
+string is_portal_open()
+{
+  //Override me
+  return destination;
+}
+
+//:FUNCTION set_destination
 // Set where you go when you enter the portal.  Valid arguments are a function
-// pointer,  or a filename
+//pointer,  or a filename
 void set_destination(mixed f)
 {
-   add_method("enter", f);
+  destination = f;
+  set_method("enter", f, 0,
+             ({sprintf(enter_msg)}),
+             ({sprintf(leave_msg)}));
+}
+
+void set_portal_enter_msg(string s)
+{
+  enter_msg = s;
+  set_destination(destination);
+}
+
+void set_portal_leave_msg(string s)
+{
+  leave_msg = s;
+  set_destination(destination);
 }
 
 string get_destination()
 {
-   return query_method_destination("enter");
+  return query_method_destination("enter");
 }
 
 int direct_look_str_obj()
 {
-   return 1;
+  return 1;
 }
 
-//: FUNCTION set_look_in_desc
+//:FUNCTION set_look_in_desc
 // Set what you see when you look in the portal.
 void set_look_in_desc(string s)
 {
-   look_in_desc = s;
+  look_in_desc = s;
 }
 
 string get_look_in_desc()
 {
-   return evaluate(look_in_desc);
+  return evaluate(look_in_desc);
 }
 
 string look_in(string prep)
 {
-   return evaluate(look_in_desc);
+  return evaluate(look_in_desc);
 }
 
 mapping lpscript_attributes()
 {
-   return (["look_in_desc":({LPSCRIPT_STRING, "setup", "set_look_in_desc"})]);
+  return (["look_in_desc":({LPSCRIPT_STRING, "setup", "set_look_in_desc"})]);
 }
 
-varargs void mudlib_setup(string destination)
+varargs void mudlib_setup(string d)
 {
-   ::mudlib_setup();
-   set_id("portal");
-   set_attached();
-   set_gettable("#It's a portal not a portable.");
-   if (destination)
-      add_method("enter", destination);
+  ::mudlib_setup();
+  set_id("portal");
+  set_attached();
+  set_gettable("#It's a portal not a portable.");
+  if (d) set_destination(d);
 }
