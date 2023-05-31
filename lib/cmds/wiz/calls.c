e7e93f18f405eafe135547bfd4632fbd3bccb9bf
Tsathoggua of NKai <sstaunp+github@gmail.com>
2023-05-31 19:18:32 +0000

    More clever 'calls' cmd.

--- lib/cmds/wiz/calls.c
+++ lib/cmds/wiz/calls.c
@@ -6,16 +6,18 @@
 //: COMMAND
 // USAGE:  calls
 //
-// This command shows you the number of call_outs that are active.
+// This command shows you the number of call_outs that are active. If a lot
+// of calls exist on same object to same function they will be abbreviated.
 //
 // Produces display like :
 //      calls
 //
 // object                                 Function            Delay
 //-----------------------------------------------------------------------------
-/// secure/master                         <function>          17m 13s
-/// domains/std/harry#474                 <function>          19h 23m 53s
-/// domains/std/harry                     <function>          3s
+// /secure/master                         <function>          17m 13s
+// /domains/std/harry#474                 <function>          19h 23m 53s
+// /domains/std/harry                     <function>          3s
+// /daemons/imud_d                        <function>          (107 calls)
 //
 // There are 3 call_outs active.
 
@@ -29,8 +31,10 @@ void main()
    mixed *call_out_stuff;
    mixed *data;
    int i;
+   mapping calls = ([]);
 
-   call_out_stuff = filter_array(call_out_info(), ( : sizeof:));
+   call_out_stuff = filter_array(call_out_info(), (
+                                                      : sizeof:));
 
    // We do not have any ANSI here, but also don't want emojis.
    set_output_flags(NO_ANSI);
@@ -41,12 +45,47 @@ void main()
    foreach (data in call_out_stuff)
    {
       if (functionp(data[1]))
-         outf("%-70O%-10s\n", CHOMP(data[1]), time_to_string(data[2], 1));
+      {
+         // outf("%-70O%-10s\n", CHOMP(data[1]), time_to_string(data[2], 1));
+         if (!calls["function"])
+            calls["function"] = ([]);
+         if (!calls["function"][data[1]])
+            calls["function"][data[1]] = ({});
+         calls["function"][data[1]] += ({time_to_string(data[2], 1)});
+      }
       else if (data[0])
-         outf("%-45s%-25s%-10s\n", CHOMP(file_name(data[0])), data[1], time_to_string(data[2], 1));
+      {
+         // outf("%-45s%-25s%-10s\n", CHOMP(file_name(data[0])), data[1], time_to_string(data[2], 1));
+         if (!calls[file_name(data[0])])
+            calls[file_name(data[0])] = ([]);
+         if (!calls[file_name(data[0])][data[1]])
+            calls[file_name(data[0])][data[1]] = ({});
+         calls[file_name(data[0])][data[1]] += ({time_to_string(data[2], 1)});
+      }
       else
-         outf("%-45s%-25s%-10s\n", "<destructed>", data[1], time_to_string(data[2], 1));
+      {
+         // outf("%-45s%-25s%-10s\n", "<destructed>", data[1], time_to_string(data[2], 1));
+         if (!calls["<destructed>"])
+            calls["<destructed>"] = ([]);
+         if (!calls["<destructed>"][data[1]])
+            calls["<destructed>"][data[1]] = ({});
+         calls["<destructed>"][data[1]] += ({time_to_string(data[2], 1)});
+      }
    }
+
+   foreach (string ob, mapping funcs in calls)
+   {
+      foreach (string f, string *t in funcs)
+      {
+         if (sizeof(t) == 1)
+         {
+            outf("%-45s%-25s%-10s\n", CHOMP(ob), f, t[0]);
+         }
+         else
+            outf("%-45s%-25s%-10s\n", CHOMP(ob), f, "("+sizeof(t) + " calls)");
+      }
+   }
+
 
    outf("\nThere are %d call_outs active.\n", sizeof(call_out_stuff));
 }
