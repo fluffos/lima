c3935ea30ad26e77e9c06913b6e06c4a5ba35222
Tsathoggua of NKai <sstaunp+github@gmail.com>
2023-11-26 19:34:43 +0100

    Elevator button naming consistency.

--- lib/std/elevator.c
+++ lib/std/elevator.c
@@ -112,7 +112,7 @@ void handle_press(string dest)
    }
 
    if (sscanf(dest, "%s/%s", key, newdest) == 2)
-      this_body()->simple_action("$N $vpress the '" + (key ? "(" + key + ") " : "") + capitalize(newdest) +
+      this_body()->simple_action("$N $vpress the '" + (key ? "[" + key + "] " : "") + capitalize(newdest) +
                                  "' button.\n");
    else
       this_body()->simple_action("$N $vpress the '" + capitalize(dest) + "' button.\n");
